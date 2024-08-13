fbb8d70f746a568f06b82f916bfed624ee8522db
Balamathias <balamathias05@gmail.com>
2024-08-13 14:50:39 +0100

    new file user.py created

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
 
@@ -23,5 +24,5 @@ class User(Base):
     session_id = Column(String, nullable=True)
     reset_token = Column(String, nullable=True)
 
-    # def __repr__(self):
-    #     return f"User<@{self.id} | {self.email}>"
+    def __repr__(self):
+        return f"User<@{self.id} | {self.email}>"
