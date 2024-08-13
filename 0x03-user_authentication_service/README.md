cfa27cc8a549e2471577cb65e1ce6c363e739e19
Balamathias <balamathias05@gmail.com>
2024-08-13 14:47:32 +0100

    new file user.py created

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,10 @@
+# User Authentication service in Flask
+
+- User sessions
+- SQLAlchemy
+
+```py
+def User() -> any:
+    return {}
+
+```
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""User Table in Flask and SQLAlchemy"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User table declaration in SQLAlchemy
+    >>> id: int
+    >>> email: str
+    >>> hashed_password: str
+    >>> session_id: str
+    >>> reset_token: str
+    """
+
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
+
+    def __repr__(self):
+        return f"User<@{self.id} | {self.email}>"
